b7a9b35b31b8ebfcea33f8eacb8a0fbc32c923e2
aparnaVihaan <120161465+aparnaVihaan@users.noreply.github.com>
2022-12-09 11:47:54 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,9 @@
 export default function Header({ title }) {
   return <h1 className="title">{title}</h1>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <h1 className="title"><title>SM Widget Example</title></h1>
+
+  <script src="https://static.soulmachines.com/widget-snippet.min.js" data-sm-greeting="Welcome" data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLWFjY2VudHVyZS0tbXlmaXJzdGRpZ2l0YWxwZXJzb24iLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV9hOGM5YzczNC01MjQyLTQxYjAtODkzNi0yMzEyYjM3OGRiMmUifQ=="></script> 
 }
